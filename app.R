be2cfe9ff689c133927a3a92cba0d7aa2a2c71ad
mmykins <mmykins@vols.utk.edu>
2019-11-13 18:39:41 -0500

    found a more appropriate format for uploading csv and then plotting the data

--- app.R
+++ app.R
@@ -1,48 +1,96 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-library(tidyverse)
-library(DT)
+library(datasets)
 
-# Define UI
 ui <- shinyUI(fluidPage(
-    
-    fileInput('target_upload', 'Choose file to upload',
-              accept = c(
-                  'text/csv',
-                  'text/comma-separated-values',
-                  '.csv'
-              )),
-    radioButtons("separator","Separator: ",choices = c(";",",",":"), selected=";",inline=TRUE),
-    DT::dataTableOutput("sample_table")
+    titlePanel("Column Plot"),
+    tabsetPanel(
+        tabPanel("Upload File",
+                 titlePanel("Uploading Files"),
+                 sidebarLayout(
+                     sidebarPanel(
+                         fileInput('file1', 'Choose CSV File',
+                                   accept=c('text/csv', 
+                                            'text/comma-separated-values,text/plain', 
+                                            '.csv')),
+                         
+                         # added interface for uploading data from
+                         # http://shiny.rstudio.com/gallery/file-upload.html
+                         tags$br(),
+                         checkboxInput('header', 'Header', TRUE),
+                         radioButtons('sep', 'Separator',
+                                      c(Comma=',',
+                                        Semicolon=';',
+                                        Tab='\t'),
+                                      ','),
+                         radioButtons('quote', 'Quote',
+                                      c(None='',
+                                        'Double Quote'='"',
+                                        'Single Quote'="'"),
+                                      '"')
+                         
+                     ),
+                     mainPanel(
+                         tableOutput('contents')
+                     )
+                 )
+        ),
+        tabPanel("First Type",
+                 pageWithSidebar(
+                     headerPanel('My First Plot'),
+                     sidebarPanel(
+                         
+                         # "Empty inputs" - they will be updated after the data is uploaded
+                         selectInput('xcol', 'X Variable', ""),
+                         selectInput('ycol', 'Y Variable', "", selected = "")
+                         
+                     ),
+                     mainPanel(
+                         plotOutput('MyPlot')
+                     )
+                 )
+        )
+        
+    )
 )
 )
 
-# Define server logic
-server <- shinyServer(function(input, output) {
+server <- shinyServer(function(input, output, session) {
+    # added "session" because updateSelectInput requires it
     
-    df_products_upload <- reactive({
-        inFile <- input$target_upload
-        if (is.null(inFile))
-            return(NULL)
-        df <- read.csv(inFile$datapath, header = TRUE,sep = input$separator)
+    
+    data <- reactive({ 
+        req(input$file1) ## ?req #  require that the input is available
+        
+        inFile <- input$file1 
+        
+        # tested with a following dataset: write.csv(mtcars, "mtcars.csv")
+        # and                              write.csv(iris, "iris.csv")
+        df <- read.csv(inFile$datapath, header = input$header, sep = input$sep,
+                       quote = input$quote)
+        
+        
+        # Update inputs (you could create an observer with both updateSel...)
+        # You can also constraint your choices. If you wanted select only numeric
+        # variables you could set "choices = sapply(df, is.numeric)"
+        # It depends on what do you want to do later on.
+        
+        updateSelectInput(session, inputId = 'xcol', label = 'X Variable',
+                          choices = names(df), selected = names(df))
+        updateSelectInput(session, inputId = 'ycol', label = 'Y Variable',
+                          choices = names(df), selected = names(df)[2])
+        
         return(df)
     })
     
-    output$sample_table<- DT::renderDataTable({
-        df <- df_products_upload()
-        DT::datatable(df)
+    output$contents <- renderTable({
+        data()
     })
     
-}
-)
+    output$MyPlot <- renderPlot({
+        x <- data()[, c(input$xcol, input$ycol)]
+        plot(x)
+        
+    })
+})
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
